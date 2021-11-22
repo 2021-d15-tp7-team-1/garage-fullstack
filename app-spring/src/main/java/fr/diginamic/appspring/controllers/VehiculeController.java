c8d4eae24d378216e798f83dab402d36f722e966
Jordan Troadec <60313387+j3dec@users.noreply.github.com>
2021-11-22 11:13:17 +0100

    Modif Stock

--- app-spring/src/main/java/fr/diginamic/appspring/controllers/VehiculeController.java
+++ app-spring/src/main/java/fr/diginamic/appspring/controllers/VehiculeController.java
@@ -1,19 +1,13 @@
 package fr.diginamic.appspring.controllers;
 
-import fr.diginamic.appspring.dao.DaoClient;
-import fr.diginamic.appspring.dao.DaoVehicule;
-import fr.diginamic.appspring.entities.ElemStock;
 import fr.diginamic.appspring.entities.Vehicule;
-import fr.diginamic.appspring.enums.EtatVehicule;
-import fr.diginamic.appspring.form.VehiculeForm;
-import fr.diginamic.appspring.repository.CrudStockRepo;
 import fr.diginamic.appspring.repository.CrudVehiculeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
 
@@ -39,7 +33,7 @@ public class VehiculeController {
 //        return cs.findAll();
 //    }
 
-//    @PostMapping("/addV")
+    //    @PostMapping("/addV")
 //    public String vechiculeCreate(@Valid @RequestBody Vehicule v) {
 //        /**
 //         * Transactionnel avec commit ou rollback
@@ -61,4 +55,24 @@ public class VehiculeController {
         return "redirect:/vehicules/list";
     }
 
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Vehicule v = cs.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("vehicule", v);
+        return "vehicules/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, @Valid Vehicule v,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            v.setId(id);
+            return "vehicules/update";
+        }
+
+        cs.save(v);
+        return "redirect:/vehicules/list";
+    }
 }
