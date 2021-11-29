8b9e744d6a58621dd356763e9ec591cb3c861161
Mathis AGBANRIN <mathis.agb@gmail.com>
2021-11-29 14:15:08 +0100

    merge branche floriane

--- app-spring/src/main/java/fr/diginamic/appspring/controllers/ClientController.java
+++ app-spring/src/main/java/fr/diginamic/appspring/controllers/ClientController.java
@@ -12,6 +12,10 @@ import org.springframework.ui.Model;
 import fr.diginamic.appspring.repository.CrudClientRepository;
 import fr.diginamic.appspring.entities.Client;
 
+/**
+ * Controller MVC pour gérer les clients
+ *
+ */
 @Controller
 @RequestMapping("/clients/listeClients")
 public class ClientController {
@@ -19,6 +23,12 @@ public class ClientController {
     @Autowired
     CrudClientRepository cl;
 
+    /**
+     * Récupère et affiche la liste des clients
+     * @param model
+     * @return "clients/listeClients"
+     * 
+     */
     @GetMapping
     public String findAll(Model model) {
         model.addAttribute("clients", (List<Client>) cl.findAll());
@@ -27,6 +37,11 @@ public class ClientController {
         return "clients/listeClients";
     }
 
+    /**
+     * Récupère et affiche le formulaire d'ajout d'un nouveau client
+     * @param model
+     * @return "clients/add"
+     */
     @GetMapping("/add")
     public String addC(Model model) {
         model.addAttribute("creationClient", new Client());
@@ -34,12 +49,25 @@ public class ClientController {
         return "clients/add";
     }
 
+    /**
+     * Traite les données du formulaire d'ajout d'un client et l'ajoute à la liste
+     * des clients
+     * @param model
+     * @param creationClient
+     * @return "redirect:/clients/listeClients"
+     */
     @PostMapping("/add")
     public String add(Model model, @ModelAttribute("creationClient") @Valid Client creationClient) {
         cl.save(creationClient); // ajout d'un nouveau client dans la base
         return "redirect:/clients/listeClients";
     }
 
+    /**
+     * Récuèpère et affiche les données d'un client à partir d'un identifiant
+     * @param model
+     * @param pid
+     * @return "clients/accesClient"
+     */
     @GetMapping("/accesClient/{id}")
     public String findById(Model model, @PathVariable("id") Long pid) {
         Client c = cl.findById(pid).get();
@@ -47,6 +75,13 @@ public class ClientController {
         return "clients/accesClient";
     }
 
+    /**
+     * Récupère et affiche le formulaire de modification d'un client en fonction de
+     * son id
+     * @param model
+     * @param dip
+     * @return "/clients/modificationClient"
+     */
     @GetMapping("/modification/{id}")
     public String modifClient(Model model, @PathVariable("id") Long dip) {
         Client d = cl.findById(dip).get();
@@ -55,6 +90,12 @@ public class ClientController {
 
     }
 
+    /**
+     * Traite les données de modification du client
+     * @param model
+     * @param clientDip
+     * @return "redirect:/clients/listeClients"
+     */
     @PostMapping("/modification")
     public String modifClient(Model model, @ModelAttribute("modifClient") @Valid Client clientDip) {
         cl.save(clientDip);
